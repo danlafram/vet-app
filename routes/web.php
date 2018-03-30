44feade30c2863dbdfd52689083e84a4e1548354
Daniel <dan.laframb@gmail.com>
2018-03-30 18:31:06 -0400

    Added request routes 1-15

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,52 @@ Route::get('/', function () {
 
 // Clinic routes
 Route::get('/clinics', 'ClinicController@index');
+Route::post('/clinics', 'ClinicController@store');
+Route::post('/clinics{id}', 'ClinicController@update');
+Route::get('/request1', 'ClinicController@request1');
+Route::get('/request6', 'ClinicController@request6');
 
 // Employee routes
 Route::get('/employees', 'EmployeeController@index');
+Route::post('/employees', 'EmployeeController@store');
+Route::post('/employees{id}', 'EmployeeController@update');
+Route::get('/request6', 'EmployeeController@request6');
+Route::get('/request9', 'EmployeeController@request9');
+Route::get('/request11', 'EmployeeController@request11');
 
 // Owner routes
 Route::get('/owners', 'OwnerController@index');
+Route::post('/owners', 'OwnerController@store');
+Route::post('/owners{id}', 'OwnerController@update');
+Route::get('/request3', 'OwnerController@request3');
+Route::get('/request10', 'OwnerController@request10');
+Route::get('/request12', 'OwnerController@request12');
+Route::get('/request13', 'OwnerController@request13');
 
 // Animals controller
 Route::get('/animals', 'AnimalController@index');
+Route::post('/animals', 'AnimalController@store');
+Route::post('/animals{id}', 'AnimalController@update');
+Route::get('/request2', 'AnimalController@request2');
+Route::get('/request4', 'AnimalController@request4');
+Route::get('/request7', 'AnimalController@request7');
+Route::get('/request15', 'AnimalController@request15');
 
 // Exam controller
 Route::get('/exams', 'ExamController@index');
+Route::post('/exams', 'ExamController@store');
+Route::post('/exams{id}', 'ExamController@update');
+Route::get('/request5', 'ExamController@request5');
+Route::get('/request8', 'ExamController@request8');
+Route::get('/request14', 'ExamController@request14');
 
 // Results controller
 Route::get('/results', 'ResultController@index');
+Route::post('/results', 'ResultController@store');
+Route::post('/results{id}', 'ResultController@update');
+
+// Treatments controller (Static table)
+Route::get('/treatments', 'TreatmentController@index');
+
+
+
